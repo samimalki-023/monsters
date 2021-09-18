07ef891b9fafa66031f0e9fed1668a5864167a46
unknown <l6519908@std.yildiz.edu.tr>
2021-09-18 21:02:39 +0300

    building

--- src/App.js
+++ src/App.js
@@ -1,33 +1,26 @@
 import React,{Component} from 'react';
-import logo from './logo.svg';
+import { CardList } from './components/card-list/card-list';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstein',
-          id: '123'
-        },
-        {
-          name: 'Dracula',
-          id: '124'
-        },
-        {
-          name: 'Zombie',
-          id: '125'
-        },
-      ]
+      monsters: [],
+      searchField: ''
     };
+  }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response =>  response.json())
+    .then(users => this.setState({monsters:users}));
   }
   render(){
     return(
       <div className="App">
-        {
-          this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name} </h1>)
-        }
+        <input type='search' placeholder='search monsters' onChange={e => this.setState({searchField: e.target.value})} />
+        <CardList monsters={this.state.monsters} />
+        
       </div>
     );
   }

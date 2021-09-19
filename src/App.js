8acdaea99cbdb2a71f738bfce95a06df3185b6f8
unknown <l6519908@std.yildiz.edu.tr>
2021-09-19 07:17:45 +0300

    building app.js

--- src/App.js
+++ src/App.js
@@ -16,10 +16,15 @@ class App extends Component {
     .then(users => this.setState({monsters:users}));
   }
   render(){
+    const {monsters, searchField} = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())
+      )
     return(
       <div className="App">
-        <input type='search' placeholder='search monsters' onChange={e => this.setState({searchField: e.target.value})} />
-        <CardList monsters={this.state.monsters} />
+        <input type='search' placeholder='search monsters' 
+        onChange={e => this.setState({searchField: e.target.value})} />
+        <CardList monsters={filteredMonsters} />
         
       </div>
     );
